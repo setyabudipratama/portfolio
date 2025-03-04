0ccc7d9e57c37b8d01aedbfe9f12745627509518
Setya Budi Pratama <setyabudipratam@gmail.com>
2025-03-04 13:36:36 +0700

    Update script.js

--- script.js
+++ script.js
@@ -556,6 +556,7 @@ Object.assign(slideProject.style, {
 });
 
 document.querySelector('#about .container .profil img').setAttribute('src', 'gambar/IMG_20240610_232649.webp');
+document.querySelector('footer .left img').setAttribute('src', 'gambar/IMG_20240610_232649.webp');
 
 
 // DOM
